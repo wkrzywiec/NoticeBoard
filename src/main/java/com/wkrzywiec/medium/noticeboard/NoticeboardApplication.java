25e8c09c9c0f05104a08518ba006bcf4fd50566a
Wojtek Krzywiec <wojciechkrzywiec@gmail.com>
2019-12-12 18:40:38 +0100

    init commit

---
+++ src/main/java/com/wkrzywiec/medium/noticeboard/NoticeboardApplication.java
@@ -0,0 +1,13 @@
+package com.wkrzywiec.medium.noticeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticeboardApplication.class, args);
+	}
+
+}
