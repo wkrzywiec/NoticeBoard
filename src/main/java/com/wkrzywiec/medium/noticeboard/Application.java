94d364b2941604d706a5277ad772798c8712533f
Wojtek Krzywiec <wojciechkrzywiec@gmail.com>
2019-12-22 09:20:28 +0100

    Swagger UI

--- src/main/java/com/wkrzywiec/medium/noticeboard/Application.java
+++ src/main/java/com/wkrzywiec/medium/noticeboard/Application.java
@@ -2,8 +2,10 @@ package com.wkrzywiec.medium.noticeboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
 
 	public static void main(String[] args) {
