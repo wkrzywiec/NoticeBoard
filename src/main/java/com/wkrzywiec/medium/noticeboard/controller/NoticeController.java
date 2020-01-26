925c39a8bd4da5ffab823cf2524113f9d27432e9
Wojtek Krzywiec <wojciechkrzywiec@gmail.com>
2020-01-26 22:50:34 +0100

    Abstract CrudController class

--- src/main/java/com/wkrzywiec/medium/noticeboard/controller/NoticeController.java
+++ src/main/java/com/wkrzywiec/medium/noticeboard/controller/NoticeController.java
@@ -1,66 +1,15 @@
 package com.wkrzywiec.medium.noticeboard.controller;
 
 import com.wkrzywiec.medium.noticeboard.controller.dto.NoticeDTO;
-import com.wkrzywiec.medium.noticeboard.service.NoticeService;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import com.wkrzywiec.medium.noticeboard.service.CrudService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Optional;
-
 @RestController
 @RequestMapping("/notices")
-public class NoticeController implements CrudController<NoticeDTO> {
+public class NoticeController extends CrudController<NoticeDTO> {
 
-    private final NoticeService noticeService;
-
-    public NoticeController(NoticeService noticeService) {
-        this.noticeService = noticeService;
-    }
-
-    @Override
-    @ApiOperation(value = "List all Notices")
-    public ResponseEntity<List<NoticeDTO>> getAll() {
-        return new ResponseEntity<>(noticeService.findAll(), HttpStatus.OK);
-    }
-
-    @Override
-    @ApiOperation(value = "Get Notice by its Id")
-    public ResponseEntity<NoticeDTO> getById(Long id) {
-        Optional<NoticeDTO> noticeOpt = noticeService.findById(id);
-
-        return noticeOpt.map(notice ->
-                new ResponseEntity<>(notice, HttpStatus.OK))
-                .orElse(new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
-    }
-
-    @Override
-    @ApiOperation(value = "Create a Notice")
-    public ResponseEntity<NoticeDTO> save(NoticeDTO notice) {
-        return new ResponseEntity<>(noticeService.save(notice), HttpStatus.CREATED);
-    }
-
-    @Override
-    @ApiOperation(value = "Delete a Notice by its Id")
-    public ResponseEntity<String> delete(Long id) {
-        Optional<NoticeDTO> noticeOpt = noticeService.findById(id);
-
-        return noticeOpt.map(notice ->
-                new ResponseEntity<>("Notice with id " + id + " was deleted.", HttpStatus.NO_CONTENT))
-                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND.getReasonPhrase(), HttpStatus.NOT_FOUND));
-    }
-
-    @Override
-    @ApiOperation(value = "Update an existing Notice by its Id")
-    public ResponseEntity<String> update(Long id, NoticeDTO notice) {
-        Optional<NoticeDTO> noticeOpt = noticeService.findById(id);
-        noticeOpt.ifPresent(n -> noticeService.update(id, notice));
-
-        return noticeOpt.map(n ->
-                new ResponseEntity<>("Notice with id " + id + " was updated.", HttpStatus.OK))
-                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND.getReasonPhrase(), HttpStatus.NOT_FOUND));
+    public NoticeController(CrudService<NoticeDTO> crudService) {
+        super(crudService);
     }
 }
